5c089997fe41ce3e5bba13e592c9d67bddbeb91c
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2026-04-13 10:44:51 +0000

    cpp setup

---
+++ cpp/src/main.cpp
@@ -0,0 +1,12 @@
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <iostream>
+
+int main() {
+    xt::xarray<double> a = {{1, 2}, {3, 4}};
+    xt::xarray<double> b = {{10, 20}, {30, 40}};
+
+    auto c = a + b;
+
+    std::cout << c << std::endl;
+}
