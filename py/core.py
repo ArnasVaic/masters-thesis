[feat]: Fix scaling, refactor

py/core.py
@@ -1,18 +1,50 @@
-# %%
-import numpy as np
 import yag_model as ym
-from core import solve, MOLAR_MASSES
-import matplotlib.pyplot as plt
+import numpy as np
+
+# Experimental data, element percentage by mass after 6hrs at 1400C
+P_TRUE = np.array([ 29.21, 19.37, 15.08, 24.06, 12.27])
+
+# Molar masses of elements within synthesis
+MOLAR_MASSES = np.array([ 102.0, 225.8, 553.58, 163.89, 593.62 ])
+
+def last_frame_mass_from_q(cpt: ym.QuantityCapture):
+    q_pred = np.array(cpt.q_history[:, 0])
+    return MOLAR_MASSES * q_pred
+
+def mass(state: ym.SolutionState, speciesIdx: int, disc: ym.Discretization):
+    M = MOLAR_MASSES[speciesIdx]
+    q = ym.quantity(state[speciesIdx], disc)
+    return M * q
+
+def total_mass(state: ym.SolutionState, disc: ym.Discretization) -> float:
+    return sum([ mass(state, i, disc) for i in range(5) ])
+
+def get_p_pred(ic, disc, cpt):
+    m0 = total_mass(ic, disc)
+    q_pred = last_frame_mass_from_q(cpt)
+    return 100 * q_pred / m0
+
+def loss(ic, disc, cpt):
+    p_pred = get_p_pred(ic, disc, cpt)
+    return np.mean((p_pred - P_TRUE) ** 2)
+
+def solve(mp, build_cfg):
+    cfg = build_cfg(mp)
+    ym.solve(*cfg)
+    return cfg
+
+def config_info(cfg):
+    print('')
 
 # Solver configuration for optimization
 # - Hardcoded timestep
 # - Rescaled model parameters
 # - Only save quantity before brake
 # - Parameters for 1400C at 6hrs
-def build_config(mp: ym.ModelParameters):
+def build_optimization_config(mp: ym.ModelParameters):
 
     # Diffusion scaling for numerical stability
-    D_ref = 1e-4
+    D_ref = 1e4
 
     # um (other constants are already in terms of um)
     L0 = 1.0
@@ -37,8 +69,8 @@ def build_config(mp: ym.ModelParameters):
     # parameters this doesn't matter much.
     ic = ym.build_checkerboard_initial_condition(
         disc, 
-        3.91e-14 / C0, 
-        3/5 * 3.91e-14 / C0
+        1.0, 
+        3/5
     )
     
     # We know the rough order of parameters so we can hardcode the timestep
@@ -54,60 +86,11 @@ def build_config(mp: ym.ModelParameters):
     
     # This run is only used for parameter search so we do not need to
     # capture any data other than the last quantity before braking at 6 hrs.
-    # cpt = ym.LastFrameCaptureTrigger(br)
-    cpt = ym.StrideCaptureTrigger(10)
+    cpt = ym.LastFrameCaptureTrigger(br)
 
     # For storage we only need 1 time step
-    # cp = ym.QuantityCapture(1, disc)
-    cp = ym.InMemoryFrameCapture(500, disc)
+    cp = ym.QuantityCapture(1, disc)
 
     cfg = [ disc, mp_nd, ts, br, cpt, cp, ic ]
+    config_info(cfg)
     return cfg
-# %%
-
-disc, _, _, _, _, cpt, ic = solve(
-    ym.ModelParameters(
-        [1e-6, 1e-6, 1e-6, 1e-6, 1e-6], 
-        [1e10, 1e10, 1e10]
-    ), 
-    build_config
-)
-
-# %%
-
-m0 = \
-    MOLAR_MASSES[0] * ym.quantity(ic[0], disc) + \
-    MOLAR_MASSES[1] * ym.quantity(ic[1], disc)
-
-ELEMENT_NAME_STRINGS = [
-    '$Al_2O_3$','$Y_2O_3$','$YAM$','$YAP$','$YAG$'
-]
-
-m_sum = np.zeros((cpt.size))
-for i in range(5):
-    qi = np.array([ ym.quantity(cpt.c_history[i, f, :, :], disc) for f in range(cpt.size) ])
-    m = qi * MOLAR_MASSES[i]
-    m_sum += m
-    plt.plot(cpt.t_history[:cpt.size], 100 * m / m0, label=ELEMENT_NAME_STRINGS[i])
-
-plt.plot(cpt.t_history[:cpt.size], 100 * m_sum / m0, label='$\\Sigma$')
-plt.xlabel('Reakcijos laikas (s)')
-plt.ylabel('Medžiagų masės dalys (%)')
-plt.legend()
-plt.tight_layout()
-
-# %%
-
-frame = 360
-assert frame < cpt.size
-species = 4
-plt.title(f"$c_{1+species}(t={cpt.t_history[frame]})$")
-
-print(f"min = {np.min(cpt.c_history)}, max = {np.max(cpt.c_history)}")
-mx = np.max(cpt.c_history)
-
-plt.imshow(cpt.c_history[species, frame, :, :], vmin=0.0, vmax=mx)
-#plt.imshow(cpt.c_history[species, frame, :, :])
-
-plt.colorbar()
-# %%

py/find_params.py
@@ -1,96 +1,14 @@
 # %%
 
-from dataclasses import dataclass
-import math
-import time
-import numpy as np
 import yag_model as ym
-import matplotlib.pyplot as plt
 import optuna
-
-P_TRUE = np.array([ 29.21, 19.37, 15.08, 24.06, 12.27])
-MOLAR_MASSES = np.array([ 102.0, 225.8, 553.58, 163.89, 593.62 ])
-
-def scale_parameters(mp, L0, T0, C0):
-    mp_nd = ym.ModelParameters()
-
-    mp_nd.D = np.array([D * T0 / (L0**2) for D in mp.D])
-    mp_nd.K = np.array([k * C0 * T0 for k in mp.K])
-
-    return mp_nd
-
-def calculate_m_total(state: ym.SolutionState, disc: ym.Discretization) -> float:
-    m_total = 0
-    for species in range(5):
-        M = MOLAR_MASSES[species]
-        q = ym.quantity(state[species], disc)
-        m_total += M * q
-    return m_total
-
-def build_cfg(mp: ym.ModelParameters):
-    L0 = 1e-6
-    T0 = L0**2 / mp.D[0]
-    C0 = 3.91e-14
-    # mp_nd = scale_parameters(mp, L0, T0, C0)
-
-    mp_nd = ym.ModelParameters(
-        [D for D in mp.D],
-        [k * C0 * T0 for k in mp.K]
-    )
-
-    disc = ym.Discretization(1e-6, 1e-6, 40, 40) #
-    ic = ym.build_checkerboard_initial_condition(disc, 3.91e-14 / C0, 3/5 * 3.91e-14 / C0)
-
-    # ts = ym.FixedTimeStep(0.01 * disc.dx * disc.dx / mp.D[0])
-    ts = ym.FixedTimeStep(6) # 6 seconds is a good rough time step that keeps simulation stable
-
-    t_end = 6 * 60 * 60
-    br = ym.TimeBrake(t_end)
-    
-    cpt = ym.LastFrameCaptureTrigger(br)
-    # target_frames = 1000
-    # frm_stride = math.ceil(t_end / (ts.dt * target_frames))
-    # cpt = ym.StrideCaptureTrigger(frm_stride)
-
-    cp = ym.QuantityCapture(1, disc)
-    # cp = ym.QuantityCapture(1000, disc)
-    # cp = ym.InMemoryFrameCapture(1000, disc)
-
-    return [ disc, mp_nd, ts, br, cpt, cp, ic ]
-
-def solve(mp, build_cfg):
-    cfg = build_cfg(mp)
-    ym.solve(*cfg)
-    return cfg
-
-def loss(ic, disc, cpt):
-    m_total = calculate_m_total(ic, disc)
-    q_pred = np.array(cpt.q_history[:, 0])
-    p_pred = 100 * MOLAR_MASSES * q_pred / m_total
-    return np.mean((p_pred - P_TRUE) ** 2)
-
-def cost(mp, build_cfg):
-    cfg = solve(mp, build_cfg)
-    return loss(cfg)
+from core import solve, build_optimization_config, loss
 
 def objective(trial):
-
-    logD1 = trial.suggest_float("logD1", -18, -16)
-    logD2 = trial.suggest_float("logD2", -18, -16)
-    logD3 = trial.suggest_float("logD3", -18, -16)
-    logD4 = trial.suggest_float("logD4", -18, -16)
-    logD5 = trial.suggest_float("logD5", -18, -16)
-
-    logk1 = trial.suggest_float("logk1", 3, 5)
-    logk2 = trial.suggest_float("logk2", 3, 5)    
-    logk3 = trial.suggest_float("logk3", 3, 5)
-    
-    mp = ym.ModelParameters(
-        [10**logD1, 10**logD2, 10**logD3, 10**logD4, 10**logD4],
-        [10**logk1, 10**logk2, 10**logk3]
-    )
-    disc, _, _, _, _, cpt, ic = solve(mp, build_cfg)
-
+    D = [10**trial.suggest_float(f"logD{i}", -7, -5) for i in range(1, 6)]
+    k = [10**trial.suggest_float(f"logk{i}", 9, 11) for i in range(1, 4)]
+    mp = ym.ModelParameters(D, k)
+    disc, _, _, _, _, cpt, ic = solve(mp, build_optimization_config)
     return loss(ic, disc, cpt)
 
 study = optuna.create_study(
@@ -102,12 +20,4 @@ study = optuna.create_study(
 
 study.optimize(objective, n_trials=100)
 
-
-
-# %%
-
-from optuna.visualization import plot_optimization_history
-
-fig = plot_optimization_history(study)
-fig.show()
 # %%

py/test_params.py
@@ -1,88 +1,43 @@
 # %%
 
-from dataclasses import dataclass
-import math
-import time
 import numpy as np
 import yag_model as ym
-import matplotlib.pyplot as plt
+from core import P_TRUE, build_optimization_config, solve, get_p_pred
 
-P_TRUE = np.array([ 29.21, 19.37, 15.08, 24.06, 12.27])
-MOLAR_MASSES = np.array([ 102.0, 225.8, 553.58, 163.89, 593.62 ])
-
-def calculate_m_total(state: ym.SolutionState, disc: ym.Discretization) -> float:
-    m_total = 0
-    for species in range(5):
-        M = MOLAR_MASSES[species]
-        q = ym.quantity(state[species], disc)
-        m_total += M * q
-    return m_total
-
-def build_cfg(mp: ym.ModelParameters):
-    L0 = 1e-6
-    T0 = L0**2 / mp.D[0]
-    C0 = 3.91e-14
-    mp_nd = ym.ModelParameters(
-        [D for D in mp.D],
-        [k * C0 * T0 for k in mp.K]
-    )
-    disc = ym.Discretization(1e-6, 1e-6, 40, 40) #
-    ic = ym.build_checkerboard_initial_condition(disc, 3.91e-14 / C0, 3/5 * 3.91e-14 / C0)
-    # 6 seconds is a good rough time step that keeps simulation stable
-    ts = ym.FixedTimeStep(6) 
-    t_end = 6 * 60 * 60
-    br = ym.TimeBrake(t_end)
-    cpt = ym.LastFrameCaptureTrigger(br)
-    # target_frames = 1000
-    # frm_stride = math.ceil(t_end / (ts.dt * target_frames))
-    # cpt = ym.StrideCaptureTrigger(frm_stride)
-    cp = ym.QuantityCapture(1, disc)
-    # cp = ym.QuantityCapture(1000, disc)
-    # cp = ym.InMemoryFrameCapture(1000, disc)
-    return [ disc, mp_nd, ts, br, cpt, cp, ic ]
-
-def solve(mp, build_cfg):
-    cfg = build_cfg(mp)
-    ym.solve(*cfg)
-    return cfg
-
-def loss(ic, disc, cpt):
-    m_total = calculate_m_total(ic, disc)
-    q_pred = np.array(cpt.q_history[:, 0])
-
-    print(q_pred.shape)
-
-    p_pred = 100 * MOLAR_MASSES * q_pred / m_total
-
-    print(f"{'Species':<10} {'Predicted':<10} {'True':<10}")
-    print("-" * 20)
-    pp_sum, pt_sum = 0, 0
+def info(p_pred):
+    print(f"{'channel':<8} {'mass % (pred)':<16} {'mass % (true)':<16} {'rel. err. %':<16} ")
+    print("-" * 56)
+    pp_sum, pt_sum, rel_err_sum = 0, 0, 0
     for i, (pp, pt) in enumerate(zip(p_pred, P_TRUE)):
         pp_sum += pp
         pt_sum += pt
-        print(f"c_{i+1:<8} {pp:<10.2f} {pt:<10.2f}")
+        rel_err = 100 * np.abs(pp - pt) / pt
+        rel_err_sum += rel_err 
+        print(f"c_{i+1:<8} {pp:<16.2f} {pt:<16.2f} {rel_err:<16.2f}")
 
-    print("-" * 35)
-    print(f"{'total':<10} {pp_sum:<10.2f} {pt_sum:<10.2f}")
+    print("-" * 56)
+    print(f"{'total':<8} {pp_sum:<16.2f} {pt_sum:<16.2f} {rel_err_sum:<16.2f}")
 
-    return np.mean((p_pred - P_TRUE) ** 2)
+logD = [
+    -5.951425472226097,
+    -6.381854122397745,
+    -6.796653711570672,
+    -6.378758235770181,
+    -5.190091649510689
+]
+
+logK = [
+    10.990986955550355,
+    10.483942406247088,
+    9.04041247830719
+]
 
 mp = ym.ModelParameters(
-    [
-        10**-17.151528159149557,
-        10**-17.68117146705129,
-        10**-17.540303191988034,
-        10**-16.97125397545409,
-        10**-17.524169541373478,
-    ],
-    [
-        10**4.196087053883452,
-        10**4.814049920233035,
-        10**3.6630618938287576,
-    ]
+    [ 10**d for d in logD ],
+    [ 10**k for k in logK ]
 )
 
-disc, _, _, _, _, cpt, ic = solve(mp, build_cfg)
-
-loss(ic, disc, cpt)
+disc, _, _, _, _, cpt, ic = solve(mp, build_optimization_config)
+p_pred = get_p_pred(ic, disc, cpt)
+info(p_pred)
 # %%