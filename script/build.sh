37e80b20787ebb3012e6dd3e01e51472afe72eab
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2026-09-01 23:08:15 +0300

    [chore]: Update build script to generate stubs for yag model module for better intellisense (yay!!!)

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 # Build Python buildings for C++ solver
 pip install -e . --no-build-isolation
+
+# Build stubs for better intellisense
+# (I love that this exists)
+# TODO: don't hardcode module name in case I wanna change it
+cd py
+pybind11-stubgen yag_model
